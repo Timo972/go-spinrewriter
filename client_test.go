d79a50d8bd3bd0d745a81032ebf3965efdbea3e3
tbeck <beckmann.timo@icloud.com>
2023-11-09 16:39:25 +0100

    chore: setup test env

--- client_test.go
+++ client_test.go
@@ -1,12 +1,20 @@
 package spinrewriter_test
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"testing"
 
+	"github.com/joho/godotenv"
 	"github.com/timo972/spinrewriter"
 )
+
+func TestMain(m *testing.M) {
+	godotenv.Load()
+	code := m.Run()
+	os.Exit(code)
+}
 
 func TestNew(t *testing.T) {
 	client := spinrewriter.New("email", "api_key")
@@ -14,6 +22,17 @@ func TestNew(t *testing.T) {
 	if client == nil {
 		t.Error("Expected client to be instance of spinrewriter.Client")
 	}
+}
+
+func TestClient_Quota(t *testing.T) {
+	client := spinrewriter.New(os.Getenv("SPINREWRITER_EMAIL"), os.Getenv("SPINREWRITER_API_KEY"))
+
+	quota, err := client.Quota()
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Println("Quota:", quota)
 }
 
 func ExampleClient_Quota() {
